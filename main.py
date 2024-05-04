de030988b614cb7731608aebe487f0cf1d3c3a32
Eric  Zhou <ericzhou@Erics-MacBook-Air-2.local>
2024-05-03 23:54:10 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ from mlp import run_mlp
 from knn_reg import  run_knn
 from svr import  run_svr
 from my_xgboost import  run_xgboost
+from rforest import run_random_forest
+
 import os
 
 
 if __name__ == '__main__':
+    #To run a specific model uncomment the model you want to run
     #run_baseline()
     #run_poly()
     #run_mlp()
     #run_knn()
-    run_svr()
+    #run_svr()
     #run_xgboost()
+    #run_random_forest
+    
+
 
 
